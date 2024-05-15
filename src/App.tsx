6e1aadec15457b2d3399c1a400658a154f5f8ba9
Nathaniel Chavez <nathanielbchavez@gmail.com>
2024-05-15 17:39:03 +0800

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
-  return (
-    <div className='App'>
-      <h1>Welcome to the Full Stack course</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
