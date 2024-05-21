4cbc61b0c9561bf9c1b609a8ae0016429125893b
Nathaniel Chavez <nathanielbchavez@gmail.com>
2024-05-21 13:09:25 +0800

    Added admin response to questions

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import LoginWidget from './Auth/LoginWidget';
 import { oktaConfig } from './lib/oktaConfig';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
+import { MessagesPage } from './layouts/MessagesPage/MessagesPage';
+import { ManageLibraryPage } from './layouts/ManageLibraryPage/ManageLibraryPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -59,6 +61,12 @@ export const App = () => {
 
             <SecureRoute path='/shelf'>
               <ShelfPage />
+            </SecureRoute>
+            <SecureRoute path='/messages'>
+              <MessagesPage />
+            </SecureRoute>
+            <SecureRoute path='/admin'>
+              <ManageLibraryPage />
             </SecureRoute>
           </Switch>
         </div>
